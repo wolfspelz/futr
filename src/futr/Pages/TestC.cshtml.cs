db3c32828f9ba22e4fe146bd5fea137da656562f
Heiner Wolf <git@heinerwolf.de>
2023-09-30 20:07:04 +0100

    r TestC also copied from TestU

--- src/futr/Pages/TestC.cshtml.cs
+++ src/futr/Pages/TestC.cshtml.cs
@@ -1,28 +1,27 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
-namespace futr.Pages
+namespace futr.Pages;
+
+public class TestCModel : FutrPageModel
 {
-    public class TestCModel : FutrPageModel
+    public string Id { get; private set; } = "";
+    public Models.Civilization? Civilization { get; private set; }
+
+    public TestCModel(FutrApp app) : base(app, nameof(TestCModel)) { }
+
+    public IActionResult OnGet(string? id)
     {
-        public string Id { get; private set; } = "";
-        public Models.Civilization? Civilization { get; private set; }
-
-        public TestCModel(FutrApp app) : base(app, nameof(TestCModel)) { }
-
-        public IActionResult OnGet(string? id)
-        {
-            if (id == null) {
+        if (id == null) {
+            return NotFound();
+        } else {
+            Id = id;
+            var civilization = App.Data.GetCivilization(id);
+            if (civilization == null) {
                 return NotFound();
-            } else {
-                Id = id;
-                var civilization = App.Data.GetCivilization(id);
-                if (civilization == null) {
-                    return NotFound();
-                }
-                Civilization = civilization;
             }
-
-            return Page();
+            Civilization = civilization;
         }
+
+        return Page();
     }
 }
