6f2b5cec6858e403eb9316c3c96dffad6c29fe1c
Heiner Wolf <git@heinerwolf.de>
2023-09-30 09:11:20 +0100

    routing: Test w/ Universe

--- src/futr/Pages/Test.cshtml.cs
+++ src/futr/Pages/Test.cshtml.cs
@@ -4,7 +4,9 @@ namespace futr.Pages;
 
 public class TestModel : FutrPageModel
 {
-    public string Text = "0";
+    public string Id = "_notset";
+    public Models.Universe Universe { get; private set; } = new Universe("");
+    public List<Models.Universe> Universes { get; private set; } = new();
 
     public TestModel(FutrApp app) : base(app, nameof(TestModel))
     {
@@ -13,9 +15,15 @@ public class TestModel : FutrPageModel
     public IActionResult OnGet(string? id)
     {
         if (id == null) {
-            Text = "null";
+            Id = "";
+            Universes = App.Data.GetUniverses();
         } else {
-            Text = id;
+            Id = id;
+            var universe = App.Data.GetUniverse(id);
+            if (universe == null) {
+                return NotFound();
+            }
+            Universe = universe;
         }
 
         return Page();
