d10ada492b69adb5daca5a03c524fc85fd082e0e
Heiner Wolf <git@heinerwolf.de>
2023-10-03 22:29:53 +0100

    old cards

---
+++ src/futr/Pages/Shared/_Card.cshtml.cs
@@ -0,0 +1,16 @@
+﻿namespace futr.Pages.Shared;
+
+public class Card
+{
+    public class Model
+    {
+        public string Text { get; set; } = "";
+        public string Title { get; set; } = "";
+        public string Link { get; set; } = "";
+        public string Anchor { get; set; } = "";
+        public bool TitleHasLink { get; set; } = false;
+        public bool TextHasLink { get; set; } = false;
+        public string ImgSrc { get; set; } = "";
+        public string ImgAlt { get; set; } = "";
+    }
+}
